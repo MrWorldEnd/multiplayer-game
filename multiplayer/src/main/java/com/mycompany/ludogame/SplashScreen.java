ba0cfd15c5c029bb58ba1df8094ece4edf57f270
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2022-04-07 15:13:36 +0100

    final

--- multiplayer/src/main/java/com/mycompany/ludogame/SplashScreen.java
+++ multiplayer/src/main/java/com/mycompany/ludogame/SplashScreen.java
@@ -11,13 +11,11 @@ import javax.swing.JFrame;
 public class SplashScreen {
 	   public static void main(String[] args) throws IOException {
 	        JFrame mainf = new JFrame();
-                DataManager connection = new DataManager();
 	        mainf.setBounds(10,10,1000,600);
 	        mainf.setTitle("LUDO GAME IN JAVA");
 	        mainf.setResizable(false);
 	        mainf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        LUDOMoves ludo = new LUDOMoves();
-		connection.clientinitial();
 	        ludo.setFocusable(true);
 	        ludo.addKeyListener(ludo);
 	        ludo.addMouseListener(ludo);
