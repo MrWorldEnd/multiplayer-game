d04ac034745b63717f37d6f58c2fae17b5225417
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2022-04-07 15:13:01 +0100

    final

--- multiplayer/src/main/java/com/mycompany/ludogame/SplashScreen.java
+++ multiplayer/src/main/java/com/mycompany/ludogame/SplashScreen.java
@@ -1,6 +1,11 @@
 package com.mycompany.ludogame;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import javax.swing.JFrame;
 
 public class SplashScreen {
@@ -18,5 +23,23 @@ public class SplashScreen {
 	        ludo.addMouseListener(ludo);
 	        mainf.add(ludo);
 	        mainf.setVisible(true);
+                
+                
+                String[] Socketmetadata = {"10.18.112.66","8008"};//adjust to suit computer
+                String hostName = Socketmetadata[0];
+                int portNumber = Integer.parseInt(Socketmetadata[1]);
+                try(Socket echoSocket = new Socket (hostName, portNumber);
+                PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+                LUDOMoves userInput;
+                
+                }catch (UnknownHostException e) {
+                    System.err.println("Can not find Host" + hostName);
+                    System.exit(1);
+                }catch (IOException e) {
+                    System.err.println("Could not ge I/O for he connection to " + hostName);
+                    System.exit(1);
+                }
 	   }
 }
